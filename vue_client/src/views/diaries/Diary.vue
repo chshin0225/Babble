d6184834e417f5e40513f06ca5eee0ee9d1522e1
신채린 <chaelinshin96@gmail.com>
2020-09-15 13:43:27 +0900

    [S03P22A310-203] feat/lin : Create Diary Navbar

--- vue_client/src/views/diaries/Diary.vue
+++ vue_client/src/views/diaries/Diary.vue
@@ -1,6 +1,21 @@
 <template>
-  <div>
-
+  <div class="box">
+    <div class="nav">
+      <div class="nav-container d-flex bg-pink">
+        <router-link :to="{ name: 'DiaryPhoto'}" class="view right-outline pointer">
+          <i class="fas fa-file-image fa-2x"></i>
+        </router-link>
+        <router-link :to="{ name: 'DiaryTimeline'}" class="view right-outline pointer">
+          <i class="fas fa-stream fa-2x"></i>
+        </router-link>
+        <router-link :to="{ name: 'DiaryCalendar'}" class="view pointer">
+          <i class="fas fa-calendar-alt fa-2x"></i>
+        </router-link>
+      </div>
+    </div>
+    
+    <!-- router-view -->
+    <router-view></router-view>
   </div>
 </template>
 
@@ -10,6 +25,33 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
+.box {
+  padding: 20px;
+}
+
+.nav-container {
+  border-radius: 0.5rem;
+}
+
+.fas {
+  padding: 0.5rem;
+}
+
+
+.right-outline {
+  border-right: 1px solid white; 
+}
+
+.view:link, .view:active, .view:visited {
+  color:white;
+  text-decoration: none;
+}
+
+
+.nav-container .router-link-exact-active {
+  color: #9BC7FF
+}
+
 
 </style>
---
+++ vue_client/src/views/diaries/DiaryPhoto.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h3>안녕</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DiaryPhoto'
+}
+</script>
+
+<style>
+
+</style>
