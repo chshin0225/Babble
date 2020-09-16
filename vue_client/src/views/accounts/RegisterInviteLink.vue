7c9f4fd9f53bae81c40876fdb50d7c651ed65819
cocony12 <apap8510@naver.com>
2020-09-16 21:33:48 +0900

    [S03P22A310-106] feat/cony : Update Invite Link Page

--- vue_client/src/views/accounts/RegisterInviteLink.vue
+++ vue_client/src/views/accounts/RegisterInviteLink.vue
@@ -1,29 +1,29 @@
 <template>
   <div class="background1">
-    <div class="container p-3 mt-5 bg-light-ivory signup-form">
+    <div class="container p-3 mt-5 bg-light-ivory enroll-form">
       <h3 class="color-pink">초대 코드 등록</h3>
         <!-- <div class="mt-3" style="font-size:12px; text-align:center">
           초대 코드를 입력해주세요.
         </div> -->
+      <div class="image-sect mt-5" style="text-align:center">
+        <img src="../../assets/images/accounts/envelope.png">
+      </div>
         
       <div class="input-with-label">
         <input 
-          v-model="signupData.nickName"
-          v-bind:class="{error: error.nickName, complete:!error.nickName&&signupData.nickName.length!==0}"
+          v-model="inviteLink"
           class="inputs"
-          id="nickName"
+          id="inviteLink"
           placeholder="초대 코드를 입력해주세요." 
           type="text" 
           autocapitalize="none"
           autocorrect="none"
           style="text-transform:lowercase"
         />
-        <label for="nickName"></label>
-        <div class="error-text ml-3" v-if="error.nickName">{{error.nickName}}</div>
+        <label for="inviteLink"></label>
       </div>
-
       <div class="buttons mt-5">
-        <button class="btn new-button" :class="{disabled: !isSubmit}" @click="clickSignup">링크 입력 완료</button>
+        <button class="btn btn-pink" :class="{disabled: !isSubmit}" @click="clickLink">링크 입력 완료</button>
       </div>
     </div>
   </div>
@@ -32,21 +32,10 @@
 <script>
 import { mapActions } from 'vuex'
 export default {
-  name: 'Signup',
+  name: 'RegisterInviteLink',
   data() {
     return {
-      signupData: {
-        email: "",
-        password: "",
-        passwordConfirm: "",
-        nickName: "",
-      },
-      error: {
-        email: false,
-        nickName: false,
-        password: false,
-        passwordConfirm: false,
-      },
+      inviteLink: "",
       isSubmit: false,
     };
   },
@@ -54,71 +43,34 @@ export default {
     this.component = this;
   },
   watch: {
-    signupData: {
+    inviteLink: {
       deep: true,
       handler() {
-        this.checknickNameForm();
-        this.checkEmailForm();
-        this.checkPasswordForm();
-        this.checkPasswordConfirmationForm();
+        this.checkInviteLink();
       }
     }
   },
   methods: {
-    checknickNameForm() {
-      if ( this.signupData.nickName.length > 0) {
-        this.error.nickName = false;
-      }
-      else this.error.nickName="닉네임을 입력해주세요."
-    },
-    checkEmailForm() {
-      if ( this.signupData.email.length > 0 && !this.validEmail(this.signupData.email) ) {
-        this.error.email = "올바른 이메일 형식이 아니에요"   
-      }
-      else this.error.email = false;
-    },
-    validEmail(email) {
-      // eslint-disable-next-line
-      var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-      return re.test(email);
-    },
-    checkPasswordForm() {
-      if (this.signupData.password.length > 0 && this.signupData.password.length < 8) {
-          this.error.password = "비밀번호가 너무 짧아요"
-        } else if ( this.signupData.password.length >= 8 && !this.validPassword(this.signupData.password) ) {
-          this.error.password = "영문, 숫자 포함 8 자리 이상이어야 해요.";
-        } else this.error.password = false;
-    },
-    validPassword(password) {
-      var va = /^(?=.*\d)(?=.*[a-z])(?=.*[a-zA-Z]).{8,}$/;
-      return va.test(password);
-    },
-    checkPasswordConfirmationForm() {
-      if (this.signupData.password.length >= 8 && this.validPassword(this.signupData.password)) {
-         if (this.signupData.password !== this.signupData.passwordConfirm )
-        this.error.passwordConfirm = "비밀번호가 일치하지 않아요."
-      else this.error.passwordConfirm = false;
-      }
+    
+    checkInviteLink() {
       
       // 버튼 활성화
-      if (this.signupData.nickName.length > 0 && this.signupData.email.length > 0 && this.signupData.password.length > 0 && this.signupData.passwordConfirm.length > 0){
-        let isSubmit = true;
-        Object.values(this.error).map(v => {
-          if (v) isSubmit = false;
-        });
-        this.isSubmit = isSubmit;
+      if (this.inviteLink.length > 0 ){
+        this.isSubmit = true;
+      }else{
+        this.isSubmit = false;
       }
      
     },
-    clickSignup() {
+    clickLink() {
       if ( this.isSubmit ){
-        this.signup(this.signupData)
+        this.enrollInviteLink(this.signupData)
       }
     },
     toLogin() {
       this.$router.push({name: "Login"});
     },
-    ...mapActions('accountStore', ['signup'])
+    ...mapActions('accountStore', ['enrollInviteLink'])
   }
 }
 </script>
@@ -140,59 +92,17 @@ h3 {
   padding: 10px;
   padding-left: 10px;
   padding-right: 10px;
-  margin-top: 20px;
+  margin-top: 30px;
 }
-.new-button{
-  background-color: #FEA59C;
-  color: #F8F8F8;
-  width: 100%;
+.btn{
+  width:100%;
 }
-.link-button{
-  background-color: #9BC7FF;
-  color: #F8F8F8;
-  width: 100%;
-}
-.divide {
-  width: 10%;
-  border-top: 1px solid #FEA59C;
-  margin-left: auto;
-  margin-right: auto;
-}
-.kakao {
-  background-color: #ffe812;
-  border-radius: 5px;
-  width: 70%;
-}
-.google {
-  background-color:  #FFFFFF;
-  border-radius: 5px;
-  width: 70%;
-}
-.inputs:focus {
-  border-style: none;
-  border-bottom: 2px solid #D6CBBD;
-  outline-style: none;
-}
-input[type="password"] {
-  font-family:sans-serif;
-}
-.error, .error:focus {
-  border-bottom: 2px solid rgb(250, 25, 59, 0.7); 
-}
-.error-text {
-  color: rgb(250, 25, 59, 0.7);
-  text-align: left;
-  padding-left: 30px;
-}
-.new-button:hover {
-  /*background-color: #3c755a;*/
-  background-color: #A05E58;
-  color: #F8F8F8;
-}
-.link-button:hover {
-  /*background-color: #3c755a;*/
-  background-color: #5C83B4;
-  color: #F8F8F8;
+.disabled,
+.disabled:hover {
+  /*background-color: rgb(136, 154, 152, 0.25);*/
+  background-color: rgb(176, 127, 122, 0.25) !important;
+  color: #f8f8f8 !important;
+  cursor: inherit !important;
 }
 .background {
   background-image: url("https://user-images.githubusercontent.com/25967949/90751489-27ce4480-e311-11ea-93aa-2ab9d1f41b4e.png");
@@ -203,12 +113,13 @@ input[type="password"] {
   right: 0;
   background-repeat: repeat;
 }
-.signup-form {
-  margin-top: 15vh !important;
+.enroll-form {
+  margin-top: 10vh !important;
   opacity: 0.9;
 }
-.items:hover {
-  cursor: pointer;
-  color: #d6cbbd;
+
+.image-sect img {
+  max-width: 40vw;
+  height: auto;
 }
 </style>
