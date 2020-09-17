184de567bfe121097c7a5ad354ea53feea536aac
신채린 <chaelinshin96@gmail.com>
2020-09-18 04:56:31 +0900

    [S03P22A310-212] feat/lin : Update router

--- vue_client/src/router/index.js
+++ vue_client/src/router/index.js
@@ -162,7 +162,34 @@ Vue.use(VueRouter)
 const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
-  routes
+  routes,
+  scrollBehavior (to, from, savedPosition) {
+    if (savedPosition) {
+      return savedPosition
+    } else {
+      return { x: 0, y: 0 }
+    }
+  }
+})
+
+router.beforeEach((to, from, next) => {
+  const publicPages = ['Login', 'Signup'] // Login 안해도 됨
+  const authPages = ['Login', 'Signup'] // Login 되어있으면 안됨
+  // const pubicPages = ['Login', 'Signup'] // Login 안해도 됨
+  // const authPages = ['Login', 'Signup'] // Login 되어있으면 안됨
+  const authRequired = !publicPages.includes(to.name) // 로그인 해야하는 페이지면 true 반환
+  const unauthRequired = authPages.includes(to.name)
+  const isLoggedIn = Vue.$cookies.isKey('auth-token')
+
+  if (unauthRequired && isLoggedIn){
+    next({name:'PhotoList'})
+  }
+  
+  if (authRequired && !isLoggedIn) {
+    next({ name: 'Login' })
+  } else {
+    next()
+  }
 })
 
 export default router
