e5f1a1f35b87462873ad976039ab9a22aa6671ea
신채린 <chaelinshin96@gmail.com>
2020-09-17 20:07:35 +0900

    [S03P22A310-123] feat/lin : Update PhotoLibrary design

--- vue_client/src/views/photos/PhotoLibrary.vue
+++ vue_client/src/views/photos/PhotoLibrary.vue
@@ -1,6 +1,12 @@
 <template>
   <div>
-    
+    <div class="text-center mt-5">
+      <img class="crying-baby" src="@/assets/baby.png">
+      <h5>
+        페이지를 개발중입니다.<br>
+        빠른 시일 내에 찾아뵙도록 하겠습니다.
+      </h5>
+    </div>
   </div>
 </template>
 
@@ -10,6 +16,9 @@ export default {
 }
 </script>
 
-<style>
-
+<style scoped>
+.crying-baby {
+  height: 50vh;
+  width: auto;
+}
 </style>
