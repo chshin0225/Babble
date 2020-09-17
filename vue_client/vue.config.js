4856b4ce5f49dd26e135db8bbc3ae5918a425d22
신채린 <chaelinshin96@gmail.com>
2020-09-17 20:56:54 +0900

    [S03P22A310-212] feat/lin : Update vue.config.js

--- vue_client/vue.config.js
+++ vue_client/vue.config.js
@@ -2,14 +2,4 @@ module.exports = {
   "transpileDependencies": [
     "vuetify"
   ],
-  css: {
-    loaderOptions: {
-        sass: {
-            data: `
-                @import "@/styles/_variables.scss";
-                @import "@/styles/_mixins.scss";
-            `
-        }
-    }
-  }
 }
