a1090f7f10500b49e45b3664a3acc71a777bc771
박선환 <witty_man@naver.com>
2020-09-18 10:43:23 +0900

    [S03P22A310-121] feat/sun: Update main.js for firebase

--- vue_client/src/main.js
+++ vue_client/src/main.js
@@ -4,19 +4,19 @@ import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify';
 import VueCookies from 'vue-cookies'
-
+import secrets from './api/secrets.json'
 // firebase
 import firebase from 'firebase'
 
 var firebaseConfig = {
-  apiKey: "AIzaSyBdMcBm_CrI9gGx0F-kjU6Q-Rxah0Q99z0",
-  authDomain: "babble-20baf.firebaseapp.com",
-  databaseURL: "https://babble-20baf.firebaseio.com",
-  projectId: "babble-20baf",
-  storageBucket: "babble-20baf.appspot.com",
-  messagingSenderId: "29022455607",
-  appId: "1:29022455607:web:5fffe19e5c4cff42af7cb2",
-  measurementId: "G-FQ6ZNW45P8"
+  apiKey: secrets['SECRET_KEY'],
+  authDomain: "babble-98541.firebaseapp.com",
+  databaseURL: "https://babble-98541.firebaseio.com",
+  projectId: "babble-98541",
+  storageBucket: "babble-98541.appspot.com",
+  messagingSenderId: "454487413592",
+  appId: "1:454487413592:web:b27b00ffc37289ba064b2d",
+  measurementId: "G-GK5WMPJZDT"
 };
 
 // Initialize Firebase
