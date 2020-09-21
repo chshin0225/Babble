7c13d12bb0114c365c9f85ad2c75e817f4a18b39
박선환 <witty_man@naver.com>
2020-09-21 14:44:39 +0900

    [S03P22A310-137] feat/sun: Add Photo Update & Delete

--- django_server/photos/views.py
+++ django_server/photos/views.py
@@ -14,7 +14,7 @@ class PhotoListView(APIView):
         cb = request.user.current_baby
         if not cb:
             raise ValueError('아이를 생성하거나 선택해주세요.')
-        photos = Photo.objects.filter(baby=cb)
+        photos = Photo.objects.filter(baby=cb).order_by('-last_modified')
         serializer = PhotoListSerializer(photos, many=True)
         return Response(serializer.data)
 
@@ -33,13 +33,32 @@ class PhotoListView(APIView):
         return Response({"message":"사진이 등록되었습니다."})
 
 class PhotoDetailView(APIView):
+    # 특정 사진 디테일 정보 조회
     def get(self, request, photo_id):
         photo = get_object_or_404(Photo, id=photo_id)
-        serializer = PhotoDetailSerializer(photo)
-        return Response(serializer.data)
+        if request.user.current_baby == photo.baby:
+            serializer = PhotoDetailSerializer(photo)
+            return Response(serializer.data)
+        else:
+            raise ValueError('해당 사진을 가져올 수 없습니다.')
+    
+    # 특정 사진 디테일 정보 수정
+    def put(self, request, photo_id):
+        cb = request.user.current_baby.id
+        if not cb:
+            raise ValueError('아이를 생성하거나 선택해주세요.')
+        photo = get_object_or_404(Photo, id=photo_id)
+        request.data["baby"] = cb
+        # 여기서 권한 검증이 한 번 들어가줘야함
+        serializer = PhotoDetailSerializer(photo, data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save(modifier=request.user)
+            return Response(serializer.data)
+        return Response(serializer.errors)
 
-    def put(self, request):
-        pass
-
-    def delete(self, request):
-        pass
+    # 특정 사진 삭제
+    def delete(self, request, photo_id):
+        photo = get_object_or_404(Photo, id=photo_id)
+        # 여기서 권한 검증이 한 번 들어가줘야함 
+        photo.delete()
+        return Response({"message":"사진이 삭제되었습니다."})
