a36adab9c35c23ddf87f7b8a40578134ea8e10c1
신채원 <chshin0225@gmail.com>
2020-09-22 14:54:45 +0900

    [S03P23A310-167] fix/chaewon: Fix response error in DiaryDetailView.get() in diaries

--- django_server/diaries/views.py
+++ django_server/diaries/views.py
@@ -3,6 +3,7 @@ from django.contrib.auth import get_user_model
 
 from rest_framework.response import Response 
 from rest_framework.views import APIView
+from rest_framework import status
 
 from .serializers import DiaryListSerializer, DiarySerializer, DiaryCommentSerializer
 from accounts.serializers import SimpleUserBabyRelationshipSerializer
@@ -45,7 +46,7 @@ class DiaryDetailView(APIView):
         return Response({
             'diary': diary_serializer.data,
             'relationship': user_baby_relationship_serializer.data
-        })
+        }, status=status.HTTP_200_OK)
 
     def put(self, request, diary_id):
         baby_id = request.user.current_baby
