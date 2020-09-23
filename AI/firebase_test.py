f9827bb54a7b1bc2edc5b135c15f5ca813263be8
방소윤 <bbangso0322@gmail.com>
2020-09-23 17:00:00 +0900

    [import pyrebase]

--- AI/firebase_test.py
+++ AI/firebase_test.py
@@ -1,21 +1,30 @@
 import pyrebase
+import requests
+from PIL import Image
+from io import BytesIO
 
 # Initialize firebase app
 config = {
-    "apiKey": "AIzaSyBdMcBm_CrI9gGx0F-kjU6Q-Rxah0Q99z0",
-    "authDomain": "babble-20baf.firebaseapp.com",
-    "databaseURL": "https://babble-20baf.firebaseio.com",
-    "projectId": "babble-20baf",
-    "storageBucket": "babble-20baf.appspot.com",
-    "messagingSenderId": "29022455607",
-    "appId": "1:29022455607:web:5fffe19e5c4cff42af7cb2",
-    "measurementId": "G-FQ6ZNW45P8"
+    "apiKey": "AIzaSyCbcpY3DVVKlUHDWW6BJHHyC_0tzUOmmRQ",
+    "authDomain": "babble-98541.firebaseapp.com",
+    "databaseURL": "https://babble-98541.firebaseio.com",
+    "projectId": "babble-98541",
+    "storageBucket": "babble-98541.appspot.com",
+    "messagingSenderId": "454487413592",
+    "appId": "1:454487413592:web:b27b00ffc37289ba064b2d",
+    "measurementId": "G-GK5WMPJZDT"
 }
+
 
 firebase = pyrebase.initialize_app(config)
 storage = firebase.storage()
 
 # Download
 # storage.child(firebase에 있는 파일 경로).download(파일 저장 이름)
-storage.child('playtime.jpg').download('playtime.jpg')
+url = storage.child('babble_15/초코.jpg').get_url(None)
 
+res = requests.get(url)
+
+img = Image.open(BytesIO(res.content))
+
+img.show()
