6d70d6ce744a48027e57793f8a305f8c173e32fa
신채원 <chshin0225@gmail.com>
2020-09-23 11:29:35 +0900

    [S03P23A310-187] refactor/chaewon: Refactor code in accounts/views.py and babies/views.py

--- django_server/accounts/urls.py
+++ django_server/accounts/urls.py
@@ -11,8 +11,12 @@ urlpatterns = [
     path('login/kakao/', views.kakao_login, name='kakao_login'),
     path('login/kakao/callback/', views.kakao_callback, name='kakao_callback'),
     path('login/kakao/todjango/', views.KakaoToDjangoLogin.as_view(), name='kako_todjango_login'),
+
+    # 현재 유저 정보 관련
     path('myinfo/', views.UserDetailView.as_view()),
     path('access/', views.BabyAccessView.as_view()),
+
+    # 현재 babble box에 속한 유저들 정보 관련
     path('groups/', views.GroupListView.as_view()),
     path('groups/<int:group_id>/', views.GroupDetailView.as_view()),
 ]
--- django_server/babies/urls.py
+++ django_server/babies/urls.py
@@ -6,6 +6,11 @@ app_name = 'babies'
 urlpatterns = [
     path('', views.BabyListView.as_view()),
     path('<int:baby_id>/', views.BabyDetailView.as_view()),
+    
+    # 한 babble box 내의 유저들 관리 관련
     path('relationships/', views.UserBabyRelationshipListView.as_view()),
+    path('relationships/<int:user_id>/', views.UserBabyRelationshipDetailView.as_view()),
+    
+    # 내가 초대된 babble box들 정보 관련
     path('mybabblebox/', views.MyBabbleBoxView.as_view()),
 ]
