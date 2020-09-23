0f5683a2a11e42dfc6d4a627ec5ec82af63ac4d1
신채린 <chaelinshin96@gmail.com>
2020-09-24 01:37:01 +0900

    [S03P23A310-195] feat/lin : Update measurement api

--- vue_client/src/api/api.js
+++ vue_client/src/api/api.js
@@ -16,6 +16,7 @@ export default {
       // diary
       diaries: '/diaries/',
       comments: '/comments/',
+      measurements: 'measurements/',
       
       // photos
       photos: '/photos/',
