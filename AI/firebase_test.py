d15816ec842c7275531873646ad3cbca85f6d153
방소윤 <bbangso0322@gmail.com>
2020-09-23 19:01:36 +0900

    [S03P23A310-381] feat/bbangso : Update gitignore

--- AI/firebase_test.py
+++ AI/firebase_test.py
@@ -1,20 +1,13 @@
 import pyrebase
 import requests
+import pickle
 from PIL import Image
 from io import BytesIO
 
 # Initialize firebase app
-config = {
-    "apiKey": "AIzaSyCbcpY3DVVKlUHDWW6BJHHyC_0tzUOmmRQ",
-    "authDomain": "babble-98541.firebaseapp.com",
-    "databaseURL": "https://babble-98541.firebaseio.com",
-    "projectId": "babble-98541",
-    "storageBucket": "babble-98541.appspot.com",
-    "messagingSenderId": "454487413592",
-    "appId": "1:454487413592:web:b27b00ffc37289ba064b2d",
-    "measurementId": "G-GK5WMPJZDT"
-}
 
+with open("config.pickle", "rb") as f:
+    config = pickle.load(f)
 
 firebase = pyrebase.initialize_app(config)
 storage = firebase.storage()
@@ -22,6 +15,7 @@ storage = firebase.storage()
 # Download
 # storage.child(firebase에 있는 파일 경로).download(파일 저장 이름)
 url = storage.child('babble_15/초코.jpg').get_url(None)
+
 
 res = requests.get(url)
 
