881bc615b2e8c0e2d2988b7cc985a82c28979d16
신채린 <chaelinshin96@gmail.com>
2020-09-25 00:18:12 +0900

    Update DiaryTimeline image length and design

--- vue_client/src/views/diaries/DiaryTimeline.vue
+++ vue_client/src/views/diaries/DiaryTimeline.vue
@@ -1,36 +1,49 @@
 <template>
   <div>
     <h5 v-if="currentBaby" class="main-title text-center color-pink">{{currentBaby.baby_name}}의 타임라인</h5>
-    <v-timeline
-      align-top
-      dense
-    >
-      <div v-for="(diary, idx) in diaries" :key="diary.id">
-        <v-timeline-item
-          :color="pickColor(idx)"
-          small
+    <div v-if="diaries">
+      <div v-if="diaries.length">
+        <v-timeline
+          align-top
+          dense
         >
-          <v-row class="pt-0">
-            <v-col cols="2" class="p-0">
-              <strong>{{diary.diary_date | moment("MM월 DD일")}}</strong>
-            </v-col>
-            <v-col cols="10" class="p-0">
-              <strong class="pointer" @click="clickDiary(diary.id)">{{diary.title}}</strong>
-              <div class="caption">by {{diary.creator}}</div>
-              <div v-if="diary.cover_photo">
-                <img 
-                  class="mt-3 pointer" 
-                  @click="clickDiary(diary.id)" 
-                  width="80vw" 
-                  :src="diary.cover_photo">
-              </div>
-              
-            </v-col>
-          </v-row>
-        </v-timeline-item>
+          <div v-for="(diary, idx) in diaries" :key="diary.id">
+            <v-timeline-item
+              :color="pickColor(idx)"
+              small
+            >
+              <v-row class="pt-0">
+                <v-col cols="2" class="p-0">
+                  <strong>{{diary.diary_date | moment("MM월 DD일")}}</strong>
+                </v-col>
+                <v-col cols="10" class="p-0">
+                  <strong class="pointer" @click="clickDiary(diary.id)">{{diary.title}}</strong>
+                  <div class="caption">by {{diary.creator.name }} ({{diary.relationship_name}})</div>
+                  <div class="image-container" v-if="diary.cover_photo">
+                    <img 
+                      class="mt-3 pointer"
+                      @click="clickDiary(diary.id)" 
+                      :src="diary.cover_photo">
+                  </div>
+                  
+                </v-col>
+              </v-row>
+            </v-timeline-item>
+          </div>
+          <div style="height: 15vh"></div>
+        </v-timeline>
       </div>
-      <div style="height: 15vh"></div>
-    </v-timeline>
+      <div v-else>
+        <div class="text-center mt-5">
+          <img class="crying-baby" src="@/assets/baby.png">
+          <h5>
+            작성된 다이어리가 아직 없습니다.<br>
+            + 버튼을 눌러 다이어리를 작성해주세요!
+          </h5>
+        </div>
+      </div>
+    </div>
+
   </div>
 
 </template>
@@ -69,5 +82,14 @@ export default {
 .main-title {
   font-weight: 900;
   font-size: 1.3rem;
+}
+
+.image-container img {
+  max-width: 50%;
+}
+
+.crying-baby {
+  height: auto;
+  width: 50vw;
 }
 </style>
