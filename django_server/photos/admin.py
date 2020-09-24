3857a826ac2027bc216ec824e209fa0264c53fec
박선환 <witty_man@naver.com>
2020-09-24 14:17:51 +0900

    [S03P23A310-189] feat/sun : Add PhotoTag CR & search

--- django_server/photos/admin.py
+++ django_server/photos/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Tag, Photo, PhotoComment, Album
+from .models import Tag, Photo, PhotoComment, Album, PhotoTag
 
 # Register your models here.
 admin.site.register(Tag)
 admin.site.register(Photo)
+admin.site.register(PhotoTag)
 admin.site.register(PhotoComment)
 admin.site.register(Album)
