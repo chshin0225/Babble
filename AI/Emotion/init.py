eb11a92f0c644c00944efe02d8dfa6abde6d3e43
이근우 <dlrmsdn135@naver.com>
2020-09-25 09:38:47 +0000

    Update init.py

--- AI/Emotion/init.py
+++
@@ -1,5 +0,0 @@
-!if not exist "./files" mkdir files
-# Download Face detection XML 
-!curl -L -o ./files/haarcascade_frontalface_default.xml https://raw.githubusercontent.com/opencv/opencv/master/data/haarcascades/haarcascade_frontalface_default.xml
-# Download emotion trained data
-!curl -L -o ./files/emotion_model.hdf5 https://mechasolution.vn/source/blog/AI-tutorial/Emotion_Recognition/emotion_model.hdf5
