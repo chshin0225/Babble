a681e50b5a32c5f8d533dfe61febd04dd247eb1e
cocony12 <apap8510@naver.com>
2020-09-28 19:40:00 +0900

    [S03P23A310-254] feat/cony : Modify Vue File Names

---
+++ vue_client/src/views/common/RankSetting.vue
@@ -0,0 +1,41 @@
+<template>
+
+  <div>
+    <div class="nav">
+      <div class="row no-gutters text-center py-3">
+        <router-link class="menu col-6" :to="{name: 'RankSettingUser'}">회원 편집</router-link>
+        <router-link class="menu col-6" :to="{name: 'RankSettingGroup'}">그룹 편집</router-link>
+      </div>
+    </div>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'GroupSetting',
+  mounted() {
+    this.$router.push({ name: 'RankSettingUser'})
+  }
+}
+</script>
+
+<style scoped>
+.menu:link, .menu:active, .menu:visited {
+  color: black;
+  text-decoration: none;
+}
+
+.menu:hover {
+  text-decoration:none;
+}
+
+.row .router-link-exact-active {
+  color: #FEA59C;
+  border-bottom: 3px solid #FEA59C;
+}
+
+
+</style>
