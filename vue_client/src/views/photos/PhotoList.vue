393b019d6758ce5e7e25cca1eb73cc6db8bf5a6b
신채린 <chaelinshin96@gmail.com>
2020-09-30 21:30:44 +0900

    [S03P23A310-338] feat/lin: Updates dates for PhotoList

--- vue_client/src/views/photos/PhotoList.vue
+++ vue_client/src/views/photos/PhotoList.vue
@@ -1,106 +1,23 @@
 <template>
   <div class="grid" data-app>
     <div v-if="photos">
-      <div class="photos row no-gutters" v-if="photos.length">
-        <div v-for="photo in photos" :key="`club_${photo.id}`" class="photo-container pointer" @click="clickPhoto(photo.id)">
-          <div class="photo">             
-            <img :src="'https://firebasestorage.googleapis.com/v0/b/babble-98541.appspot.com/o/' + photo.image_url + '?alt=media&token=fc508930-5485-426e-8279-932db09009c0'" class="card-img-top " alt="">
-          </div>
-        </div>
-      </div>
-      <div v-else class="text-center no-photos mt-5">
-         <!-- 만약 업로드 된이미지가 없을 경우 -->
-        <img class="crying-baby" src="@/assets/baby.png">
-        <h5>업로드 된 이미지가 없습니다.<br>아기 사진을 올려주세요!</h5>
-      </div>
-    </div>
-    <!-- 여기부터 가짜 -->
-    <!-- <div>
-      <h5 class="date">2020-09-15 화</h5>
-      <div class="photos d-flex">
-        <div class="photo-container pointer" @click="clickPhoto">
-          <div class="photo">
-            <img src="https://t1.daumcdn.net/tvpot/thumb/s8b90Dh8u7sDgMlccgchys3/thumb.png?ts=1541536764" class="card-img-top " alt="...">
-          </div>
-        </div>
-        <div class="photo-container">
-          <div class="photo">
-            <img src="https://pds.joins.com/news/component/htmlphoto_mmdata/201501/07/htm_20150107104932c010c011.jpg" class="card-img-top" alt="...">
-          </div>
-        </div>
-        <div class="photo-container">
-          <div class="photo">
-            <img src="https://image.ajunews.com/content/image/2015/02/25/20150225134705285589.jpg" class="card-img-top" alt="...">
+      <div class="my-3" v-for="(dates, idx) in photos" :key="idx">
+        <h5 class="dates">{{dates.date | moment("YYYY년 M월 DD일")}}</h5>
+        <div class="photos row no-gutters" v-if="photos.length">
+          <div v-for="photo in dates.photos" :key="`photo${photo.id}`" class="photo-container pointer" @click="clickPhoto(photo.id)">
+            <div class="photo">             
+              <img :src="'https://firebasestorage.googleapis.com/v0/b/babble-98541.appspot.com/o/' + photo.image_url + '?alt=media&token=fc508930-5485-426e-8279-932db09009c0'" class="card-img-top " alt="">
+            </div>
           </div>
         </div>
       </div>
     </div>
-    <div>
-      <h5 class="date">2020-09-13 일</h5>
-      <div class="photos d-flex">
-        <div class="photo-container">
-          <div class="photo">
-            <img src="https://mimg.segye.com/content/image/2015/01/30/20150130002870_0.jpg" class="card-img-top " alt="...">
-          </div>
-        </div>
-        <div class="photo-container">
-          <div class="photo">
-            <img src="https://news.imaeil.com/inc/photos/2015/03/27/2015032710112575472_m.jpg" class="card-img-top" alt="...">
-          </div>
-        </div>
-      </div>
+    <!-- 만약 업로드 된이미지가 없을 경우 -->
+    <div v-else class="text-center no-photos mt-5">
+      <img class="crying-baby" src="@/assets/baby.png">
+      <h5>업로드 된 이미지가 없습니다.<br>아기 사진을 올려주세요!</h5>
     </div>
-    <div>
-      <h5 class="date">2020-09-08 화</h5>
-      <div class="photos d-flex">
-        <div class="photo-container">
-          <div class="photo">
-            <img src="https://img1.daumcdn.net/thumb/R800x0/?scode=mtistory2&fname=https%3A%2F%2Ft1.daumcdn.net%2Fcfile%2Ftistory%2F22250C45595DFF2808" class="card-img-top " alt="...">
-          </div>
-        </div>
-        <div class="photo-container">
-          <div class="photo">
-            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSrNPbIgSPkRCQMuXvofe6jLsUWaP6eALIiFQ&usqp=CAU" class="card-img-top" alt="...">
-          </div>
-        </div>
-        <div class="photo-container">
-          <div class="photo">
-            <img src="https://image.chosun.com/sitedata/image/201503/19/2015031902832_0.jpg" class="card-img-top" alt="...">
-          </div>
-        </div>
-      </div>
-    </div>
-    <div>
-      <h5 class="date">2020-09-06 일</h5>
-      <div class="photos d-flex">
-        <div class="photo-container">
-          <div class="photo">
-            <img src="https://pds.joins.com/news/component/htmlphoto_mmdata/201501/08/htm_2015010810155c010c011.jpeg" class="card-img-top " alt="...">
-          </div>
-        </div>
-      </div>
-    </div> -->
 
-    <!-- 날짜 선택 -->
-    <!-- <h4>날짜 선택</h4>
-    <div class="date-picking">
-      <span @click.stop="dialog = true">YYYY</span>
-      <v-dialog
-        v-model="dialog"
-        max-width="290"
-      >
-        <v-row align="center">
-          <v-date-picker 
-            v-model="picker" 
-            color="green lighten-1"
-            type="month"
-            year-icon="mdi-calendar-blank"
-            prev-icon="mdi-skip-previous"
-            next-icon="mdi-skip-next"
-          ></v-date-picker>
-        </v-row>
-      </v-dialog>
-    </div> -->
     <!-- 사진 추가 버튼 구현 -->
     <div @click="clickAdd" class="add-button color-pink pointer">
       <i class="fas fa-plus-circle fa-2x"></i>
@@ -115,8 +32,7 @@ export default {
   name: 'PhotoList',
   data() {
     return {
-      // picker: new Date().toISOString().substr(0, 7),
-      // dialog: false,
+      dates: [],
     }
   },
   computed: {
@@ -142,6 +58,10 @@ export default {
 <style scoped>
 .grid {
   padding: 0 0 0 2.5vw;
+}
+
+.dates {
+  font-weight: 900;
 }
 
 .photos img {
@@ -187,3 +107,5 @@ export default {
 }
 
 </style>
+
+
