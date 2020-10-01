764c9143a83daf8c5b8ec271371bdd09867aaea1
cocony12 <apap8510@naver.com>
2020-10-01 18:10:17 +0900

    [S03P23A310-383] feat/cony : Update Baby Setting Page

--- vue_client/src/views/common/BabySetting.vue
+++ vue_client/src/views/common/BabySetting.vue
@@ -3,50 +3,148 @@
     <div style="width:100%; text-align:center">
       <img src="http://bit.do/babbleprofile" style="width:50vw; height:50vw; border-radius:50%; border:1px solid #888888;">
     </div>
-    <div class="input-with-title mt-3">
+    <!-- <div class="input-with-title mt-3">
       <div class="input-title-wrap"><span class="input-title">아기 이름/태명</span></div>
-      <div class="inputs-wrap"><input class="inputs" /></div>
+      <div class="inputs-wrap"><input class="inputs" v-model="enrollData.baby_name" /></div>
     </div>
+     -->
+    
+      <div class="mt-4 guide-text">
+        아기의 이름을 입력해 주세요.
+      </div>
+      <div class="input-with-label">
+        <input 
+          v-model="enrollData.baby_name"
+          v-bind:class="{errorText: error.baby_name, complete:!error.baby_name&&enrollData.baby_name.length!==0}"
+          class="inputs"
+          style="margin-top:0px"
+          id="baby_name"
+          placeholder="이름, 별명, 태명 등" 
+          type="text" 
+          autocapitalize="none"
+          autocorrect="none"
+        />
+        <label for="baby_name"></label>
+        <div class="error-text ml-2" v-if="error.baby_name">{{error.baby_name}}</div>
+      </div>
+
+      <div class="mt-4 guide-text">
+        아기의 성별을 선택해 주세요.
+      </div>
+      <div class="buttons" style="margin-top:10px; birth_height:auto">
+        <button class="btn girl-button" style="float:left;" :class="{selected: enrollData.gender=='F'}" @click="clickGirlBtn">여자 아기</button>
+        <button class="btn boy-button" style="float:right;" :class="{selected: enrollData.gender=='M'}" @click="clickBoyBtn">남자 아기</button>
+        <label for="gender"></label>
+        <div class="error-text ml-2" v-if="error.gender">{{error.gender}}</div>
+        <div style="float:left; width:100%; margin-bottom:30px"/>
+      </div>
+
+      <div class="mt-4 guide-text">
+        아기의 출생일자를 선택해 주세요.
+      </div>
+      <div class="input-with-label">
+        <input 
+          v-model="enrollData.birth" 
+          v-bind:class="{errorText : error.birth, complete:!error.birth&&enrollData.birth.length!==0}"
+          class="inputs"
+          id="birth" 
+          placeholder="년 월 일" 
+          type="date" 
+          autocapitalize="none"
+          autocorrect="none"
+          required
+          />
+        <label for="birth"></label>
+        <div class="error-text ml-2" v-if="error.birth">{{error.birth}}</div>
+      </div>
+
+        <div class="mt-4" style="font-size:20px; color:#bbbbbb;">
+          선택사항
+        </div>
+
+      <div class="mt-3 guide-text">
+        출생 시 아기의 키를 입력해 주세요.
+      </div>
+      <div class="input-with-label">
+        <input
+          v-model="enrollData.birth_height"
+          type="number"
+          id="birth_height"
+          placeholder="키"
+          class="inputs"
+          required
+          @keyup.enter="clickEnroll"
+        />
+        <label for="birth_height"></label>
+      </div>
+      
+        <div class="mt-4 guide-text">
+          출생 시 아기의 몸무게를 입력해 주세요.
+        </div>
+      <div class="input-with-label">
+        <input
+          v-model="enrollData.birth_weight"
+          type="number"
+          id="birth_weight"
+          placeholder="몸무게"
+          class="inputs"
+          required
+          @keyup.enter="clickEnroll"
+        />
+        <label for="birth_weight"></label>
+      </div> 
+      <!-- -->
   
     <div class="buttons mt-2">
-      <button class="btn change-button">변경하기</button>
+      <button class="btn change-button" @click="modifyBabyInfo">변경하기</button>
     </div>
     
-    <div class="input-with-title mt-5">
-      <div class="input-title-wrap"><span class="input-title">비밀번호</span></div>
-      <div class="inputs-wrap"><input class="inputs" placeholder="현재 비밀번호" v-model="old_password"/></div>
-      <div class="inputs-wrap"><input class="inputs" placeholder="새 비밀번호" v-model="new_password1"/></div>
-      <div class="inputs-wrap"><input class="inputs" placeholder="새 비밀번호 확인" v-model="new_password2"/></div>
-    </div>
-    <div class="buttons mt-2">
-      <button class="btn change-button" @click="changePW">변경하기</button>
-    </div>
   </div>
 </template>
 
 <script>
-import { mapActions } from 'vuex'
+import { mapState, mapActions } from 'vuex'
 
 export default {
   name: 'BabySetting',
   data() {
     return {
-      old_password:"",
-      new_password1:"",
-      new_password2:"",      
+      enrollData: {
+        baby_name: "",
+        gender: "",
+        birth: "",
+        birth_height: "",
+        birth_weight: "",
+        profile_image: "",
+        id: "",
+      },  
+      error: {
+        baby_name: false,
+        gender: false,
+        birth: false,
+        relationship_name: false,
+      },    
      
     }
   },
+  computed: {
+    ...mapState(['myaccount', 'currentBaby']),
+  },
+  mounted() {
+    this.enrollData = this.currentBaby;
+  },
   methods:{
-    ...mapActions('accountStore', ['changePassword']),
-    changePW(){
-      let PWData = {
-            old_password : this.old_password, 
-            new_password1 : this.new_password1, 
-            new_password2 : this.new_password2
-          }
-      this.changePassword(PWData);
-    }
+    ...mapActions('babyStore', ['modifyBaby']),
+
+    modifyBabyInfo(){
+      this.modifyBaby(this.enrollData);
+    },
+    clickGirlBtn(){
+      this.enrollData.gender = 'F';
+    },
+    clickBoyBtn(){
+      this.enrollData.gender = 'M';
+    },
   }
 }
 </script>
@@ -95,4 +193,26 @@ export default {
   width: 100%;
 }
 
+.girl-button {
+  /*background-color: #F8F8F8;*/
+  border-color: #FEA59C;
+  color: #FEA59C;
+  width: 49.5%;
+}
+.girl-button.selected{
+  background-color: #FEA59C !important;
+  color: #F8F8F8 !important;
+  width: 49.5%;
+}
+.boy-button{
+  /*background-color: #F8F8F8;*/
+  border-color: #9BC7FF;
+  color: #9BC7FF;
+  width: 49.5%;
+}
+.boy-button.selected{
+  background-color: #9BC7FF !important;
+  color: #F8F8F8 !important;
+  width: 49.5%;
+}
 </style>
