2c890d6ab9ab9bf6e2e950eae082344ea2bf2feb
cocony12 <apap8510@naver.com>
2020-10-06 18:14:25 +0900

    [S03P23A310-361] feat/cony : Update Baby Setting Page

--- vue_client/src/store/modules/babyStore.js
+++ vue_client/src/store/modules/babyStore.js
@@ -1,7 +1,7 @@
 import SERVER from '@/api/api'
 import axios from 'axios'
 //import router from '@/router'
-// import Swal from 'sweetalert2'
+import Swal from 'sweetalert2'
 //import firebase from 'firebase'
 
 const babyStore = {
@@ -16,9 +16,14 @@ const babyStore = {
   actions: {
     
     modifyBaby({ rootGetters }, enrollData) {
+      console.log("enrollData", enrollData.profile_image);
       axios.put(SERVER.URL + SERVER.ROUTES.babies + enrollData.id + '/', enrollData, rootGetters.config)
       .then(res => {
           console.log(res)
+          Swal.fire({
+            icon: 'success',
+            text: '수정되었습니다.'
+          })
       })
       .catch(err => {
           console.log(err)
