e88c8f45490d8cd692e316a67e51feac0a15b738
신채린 <chaelinshin96@gmail.com>
2020-10-07 21:43:52 +0900

    [S03P23A310-198] feat/lin : Update RankSettingUser

--- vue_client/src/views/common/RankSettingUser.vue
+++ vue_client/src/views/common/RankSettingUser.vue
@@ -1,9 +1,40 @@
 <template>
 
-  <div style="padding:0 5vw">
-    
-    <div style="width:100%;">
-        <div style="float:right;">
+  <div style="padding:0 5vw" v-if="users">
+    <!-- 공동양육자 -->
+    <div v-if="users.maintainers">
+      <div v-if="users.maintainers.length">
+        <h5>공동양육자</h5>
+        <!-- <div v-for="user in users.maintainers" :key="`maintainer_${user.id}`"> -->
+          
+          <v-list-item 
+            class="d-flex justify-content-between align-items-center"
+            v-for="userItem in users.maintainers" 
+            :key="`guest-${userItem.id}`"
+          >
+            <v-col col="3">
+              <p class="m-0 mr-3 mb-5">{{ userItem.user.name }}</p>
+            </v-col>
+            <v-col col="9">
+              <v-select
+                class="ma-0 pa-0"
+                :items="items"
+                filled
+                dense
+                label="권한"
+                v-model="userItem.rank"
+                :item-text="'label'"
+                :item-value="'value'"
+                @change="changeRank(userItem.user.id, userItem.relationship_name, userItem.rank)"
+              ></v-select>
+            </v-col>
+          </v-list-item>
+        </div>
+      </div>
+    <!-- 손님 -->
+    <div class="d-flex justify-content-between mt-3">
+      <h5>손님</h5>
+      <div>
         <button class="btn btn-outline-pink" style="" @click="selectAll">{{!isCheckAll?'전체 선택':'선택 해제'}}</button>
         <v-bottom-sheet v-model="sheet">
           <template v-slot:activator="{ on, attrs }">
@@ -17,7 +48,7 @@
             <v-subheader>그룹 목록</v-subheader>
             <v-list-item
               v-for="group in groups"
-              :key="group.id"
+              :key="`group_${group.id}`"
               @click="sheet = false"
             >
               <v-list-item-title @click="clickGroup(group.id)">{{ group.group_name }}</v-list-item-title>
@@ -26,13 +57,11 @@
         </v-bottom-sheet>
       </div>
     </div>
-    <div style="width:100%;">
-      <v-subheader>초대된 회원 목록</v-subheader>
-
+    <div v-if="users.guests">
       <v-list-item-group
         multiple
       >
-        <v-list-item v-for="userItem in users" :key="userItem.id">
+        <v-list-item v-for="userItem in users.guests" :key="`guest-${userItem.id}`">
           <template v-slot:default="{ active }">
             <v-list-item-action>
               <v-checkbox
@@ -48,16 +77,16 @@
             </v-list-item-content>
             <v-list-item-content>
               
-              <select 
+              <v-select
+                :items="items"
+                filled
+                dense
+                label="권한"
                 v-model="userItem.rank"
-                class="selects"
-                id="nickName"
+                :item-text="'label'"
+                :item-value="'value'"
                 @change="changeRank(userItem.user.id, userItem.relationship_name, userItem.rank)"
-              >
-              <option v-for="item in items" :key="item.value" :value="item.value">
-                {{ item.label }}
-              </option>
-              </select>
+              ></v-select>
             </v-list-item-content>
             <v-list-item-icon>
               <v-icon v-if="myaccount.id != userItem.user.id" color="red" @click="deleteUser(userItem.user.id)">mdi-trash-can-outline</v-icon>
@@ -88,16 +117,23 @@ export default {
     return {
       sheet:false,
       items:[
-        {label: "Master", value:"1"},
-        {label: "Maintainer", value:"2"},
-        {label: "Guest", value:"3"},
+        {label: "공동양육자", value:"2"},
+        {label: "손님", value:"3"},
       ],
       isCheckAll : false,
     }
   },
   computed: {
     ...mapState('settingStore', ['groups', 'users']),
-    ...mapState(['myaccount'])
+    ...mapState(['myaccount']),
+    findStatus(rank) {
+      if (rank === 2){
+        return '공동양육자'
+      } else {
+        return '손님'
+      }
+    },
+    
   },
   mounted() {
     this.fetchUsers();
@@ -112,10 +148,12 @@ export default {
     clickGroup(groupId){
       for(var i=0; i<this.users.length; i++){
         if(this.users[i].isCheck == true){
-        
-          let userData = {groupId : groupId, user : this.users[i].user.id};
-          console.log("userData", userData);
-          this.addUser(userData);
+          console.log(this.users[i].group)
+          if(this.users[i].group === null) {
+            let userData = {groupId : groupId, user : this.users[i].user.id};
+            console.log("userData", userData);
+            this.addUser(userData);
+          }
         }
       }
       this.isCheckAll = false;
@@ -127,12 +165,11 @@ export default {
     selectAll(){
       this.isCheckAll = !(this.isCheckAll);
       
-        for(var idx=0; idx<this.users.length; idx++){
-          this.users[idx].isCheck = this.isCheckAll;
+        for(var idx=0; idx<this.users.guests.length; idx++){
+          this.users.guests[idx].isCheck = this.isCheckAll;
         }
     },
     changeRank(userId, userRelationship, userRank){
-      
       let userData = {userId : userId, rank : userRank, relationship_name : userRelationship}
       this.modifyUserRank(userData);
     },
@@ -162,6 +199,7 @@ export default {
       });
 
     },
+
     
   },
 }
